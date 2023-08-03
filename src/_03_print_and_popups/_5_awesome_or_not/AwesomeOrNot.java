d06b098ca16e16d1e52f2ec55d8ec8daceb3b859
Student <league@iMac-22.attlocal.net>
2023-08-03 11:58:25 -0700

    Update AwesomeOrNot.java

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -7,16 +7,47 @@ import javax.swing.JOptionPane;
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
-
+public static void main(String[] args) {
 	
-		Random ran = new Random();    //This will be used below to make a random number. 
-		
-		int number = ran.nextInt(2);
-		
-		System.out.println(number);
-		
-		String input = JOptionPane.showInputDialog("Tell me something that you think is awsome"); 
-		
+	Random ran = new Random();    //This will be used below to make a random number. 
+	
+	int number = ran.nextInt(4);
+	
+	System.out.println(number);
+	
+	String input = JOptionPane.showInputDialog("Tell me something that you think is awsome"); 
+	
+	if(number == 0) {JOptionPane.showMessageDialog(null, "I also think that is awsome!");}
+	
+	if(number == 1) {JOptionPane.showMessageDialog(null, "I think that's ok");}
+	
+	if(number == 2) {JOptionPane.showMessageDialog(null, "I think thats boring");}
+	
+	if(number == 3) {JOptionPane.showMessageDialog(null, "// 2. Make a variable that will hold a random whole number\n"
+			+ "	\n"
+			+ "		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); \n"
+			+ "	\n"
+			+ "		// 3. Print your variable to the console\n"
+			+ "	\n"
+			+ "		// 4. Get the user to enter something that they think is awesome\n"
+			+ "	\n"
+			+ "		// 5. If your variable is  0\n"
+			+ "	\n"
+			+ "			// -- tell the user whatever they entered is awesome!\n"
+			+ "	\n"
+			+ "		// 6. If your variable is  1\n"
+			+ "	\n"
+			+ "			// -- tell the user whatever they entered is ok.\n"
+			+ "	\n"
+			+ "		// 7. If your variable is  2\n"
+			+ "	\n"
+			+ "			// -- tell the user whatever they entered is boring.\n"
+			+ "	\n"
+			+ "		// 8. If your variable is  3\n"
+			+ "	\n"
+			+ "			// -- invent your own message to give to the user (be nice).");}
+}
+	
 		
 		
 		
